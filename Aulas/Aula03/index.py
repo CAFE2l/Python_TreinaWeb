6a79f693a66839221296c90fb301acdbf50d9e13
CAFE2l <pciskolargx@gmail.com>
2026-02-19 18:29:02 -0300

    third class finished

---
+++ Aulas/Aula03/index.py
@@ -0,0 +1,11 @@
+import MySQLdb
+
+conn = MySQLdb.connect(
+    user="cafe",
+    passwd="SenhaForte123!",
+    db="treinaweb_clientes",
+    host="localhost",
+    port=3306
+)
+
+print("Conexão realizada com sucesso")
