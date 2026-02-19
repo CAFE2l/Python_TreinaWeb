7425368b0e24077340486aa70f82b01a7e2e97fb
CAFE2l <pciskolargx@gmail.com>
2026-02-19 14:27:31 -0300

    duvido funcionar

--- Aulas/Aula01/index.py
+++ Aulas/Aula01/index.py
@@ -1,4 +1,13 @@
-import time 
-print("Hello, World!")
-time.sleep(1)
-print("Tinha que ser gay")
+# 0 / 1 / 2 / 3 / 4 / 5
+
+numeros = list()
+tamanho = int(input("Digite o tamanho do vetor: "))
+for i in range(tamanho):
+    #0 ... 5
+    valor = int(input(f"Digite o número do vetor na posição {i}: "))
+    numeros.append(valor)
+print("Vetor: ", numeros)
+
+
+
+
