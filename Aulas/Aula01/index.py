73856199fecce7309498cd94371f50640ff1b802
CAFE2l <pciskolargx@gmail.com>
2026-02-19 16:19:33 -0300

    understanding about selection sort

--- Aulas/Aula01/index.py
+++ Aulas/Aula01/index.py
@@ -54,7 +54,7 @@ print(f"{estilos['negrito']}{cores['vermelho']}{"==="*11+"=="}{cores['limpa']}")
 
 
 # 0 / 1 / 2 / 3 / 4 
-# # 5 / 2 / 4 / 6 / 1
+# 5 / 2 / 4 / 6 / 1
 numeros = list()
 tamanho = int(input(f"{estilos['negrito']}{cores['cinza']}Digite o tamanho do vetor: "))
 for i in range(tamanho):
@@ -73,9 +73,27 @@ for i in range(tamanho):
         posicao_resultado = i
         break
 if posicao_resultado < 0: 
-    print(f"{cores['vermelho']}${estilos['italico']}O elemento não foi encontrado no vetor{cores['limpa']}")
+    print(f"{cores['vermelho']}{estilos['italico']}O elemento não foi encontrado no vetor{cores['limpa']}")
 else:
     print(f"Elemento encontrado na posição {estilos['italico']}{cores['verde']}{posicao_resultado}")
 
 # FIM BUSCA LINEAR
+# SELECTION SORT
+# 0 / 1 / 2 / 3 / 4
+# 5 / 2 / 4 / 6 / 1
+# 1 / 2 / 4 / 6 / 5
+# 1 / 2 / 4 / 5 / 6
 
+for i in range(tamanho):
+    indice_menor = i
+    for j in range(int(i + 1), tamanho):
+        if numeros[j] < numeros[indice_menor]:
+            indice_menor = j
+    temp = numeros[indice_menor]
+    numeros[indice_menor] = numeros[i]
+    numeros[i] = temp
+    print(f"vetor: {numeros}")
+
+
+
+# FIM SELECTION SORT
