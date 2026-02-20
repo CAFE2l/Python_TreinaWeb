d836e11d584595e8ea0711aa9c3a5d1e853f91de
CAFE2l <pciskolargx@gmail.com>
2026-02-20 17:20:37 -0300

    organizing the classes and methods in the path files

--- Aulas/Aula07/index.py
+++ Aulas/Aula07/index.py
@@ -1,43 +1,10 @@
-import MySQLdb, cliente
-
-db = MySQLdb.connect(
-    user="cafe",
-    passwd="SenhaForte123!",
-    db="treinaweb_clientes",
-    host="localhost",
-    port=3306,
-    autocommit=True
-)
-cursor = db.cursor()
+import MySQLdb, cliente, cliente_repositorio
 
 
-def listar_clientes():
-    cursor.execute("SELECT * FROM clientes")
-    print(cursor.fetchall())
+cliente = cliente.Cliente("ANDRADE", 23)
 
+cliente_repositorio.ClienteRepositorio.listar_clientes()
+cliente_repositorio.ClienteRepositorio.inserir_cliente(cliente)
+#cliente_repositorio.ClienteRepositorio.editar_cliente(3, cliente)
+#cliente_repositorio.ClienteRepositorio.remove_cliente(6)
 
-def inserir_cliente(cliente):
-    cursor.execute(
-        "INSERT INTO clientes (nome, idade) VALUES (%s, %s)",
-        (cliente.nome, cliente.idade)
-    )
-
-
-def editar_cliente(id_cliente, cliente):
-    cursor.execute("UPDATE clientes SET nome=%(nome)s, idade=%(idade)s WHERE idcliente=%(id_cliente)s",
-                   ({'nome': cliente.nome,
-                     'idade': cliente.idade,
-                     'id_cliente': id_cliente}))
-
-def remover_cliente(id_cliente):
-    cursor.execute("DELETE FROM clientes WHERE idcliente=%s", (id_cliente,))
-
-
-cliente = cliente.Cliente("sabrino", 29)
-
-listar_clientes()
-inserir_cliente(cliente)
-editar_cliente(3, cliente)
-remover_cliente(6)
-
-db.close()
