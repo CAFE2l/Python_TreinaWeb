74a9fcda010f6b5fadb09ec558542feb6af34e51
CAFE2l <pciskolargx@gmail.com>
2026-02-20 15:30:33 -0300

    class 4 started

--- Aulas/Aula04/index.py
+++ Aulas/Aula04/index.py
@@ -6,5 +6,19 @@ db = MySQLdb.connect(
     db="treinaweb_clientes",
     host="localhost",
     port=3306,
-    autocommit=False
+    autocommit=True
 )
+cursor = db.cursor()
+nome = "CAFÉ"
+cursor.execute(f"INSERT INTO clientes(nome, idade) VALUES('{nome}', 25)")
+cursor.execute("SELECT * FROM clientes")
+print(cursor.fetchall())
+
+nome = "carlos"
+
+#SQL injection 
+cursor.execute(f"UPDATE clientes SET nome='{nome}', idade=80  WHERE idcliente=1")
+cursor.execute("SELECT * FROM clientes")
+print(cursor.fetchall())
+
+db.close()
