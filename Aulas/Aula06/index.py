c75aa28b3f89e6332e78e69b235f99cf3901128a
CAFE2l <pciskolargx@gmail.com>
2026-02-20 15:54:51 -0300

    class 6 started

---
+++ Aulas/Aula06/index.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+db = MySQLdb.connect(
+    user="cafe",
+    passwd="SenhaForte123!",
+    db="treinaweb_clientes",
+    host="localhost",
+    port=3306,
+    autocommit=True
+)
+cursor = db.cursor()
+
+
+
