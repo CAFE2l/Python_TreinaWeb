38f3054f492183ecf2f05ddd555a2401d134426c
CAFE2l <pciskolargx@gmail.com>
2026-02-20 14:43:11 -0300

    updating clients in the database with python

--- Aulas/Aula03/:wqall.py
+++ Aulas/Aula03/:wqall.py
@@ -12,9 +12,8 @@ cursor.execute("SELECT * FROM clientes")
 print(cursor.fetchall())
 print("conexao realizda com sucesso")
 cursor.execute("INSERT INTO clientes (nome, idade) VALUES ('luis gustavo', 16)")
-
-
-
+cursor.execute("SELECT * FROM clientes")
+print(cursor.fetchall())
 print(cursor.lastrowid)
 cursor.execute("UPDATE clientes SET nome='Ana' WHERE idcliente=1")
 cursor.execute("SELECT * FROM clientes")
