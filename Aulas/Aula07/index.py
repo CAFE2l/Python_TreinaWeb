7135d84457b3e64778d388b3cbed8e5a7028551c
CAFE2l <pciskolargx@gmail.com>
2026-02-20 16:18:15 -0300

    understanding core concepts of object oriented programming

--- Aulas/Aula07/index.py
+++ Aulas/Aula07/index.py
@@ -11,5 +11,24 @@ db = MySQLdb.connect(
 cursor = db.cursor()
 
 
+def listar_clientes(self):
+    cursor.execute("SELECT * FROM clientes")
+    print(cursor.fetchall())
+
+
+def inserir_cliente(self, cliente):
+    cursor.executemany("INSER INTO clientes (nome, idade) VALUES (%s, %s)", cliente.nome, cliente.idade)
+    
+def editar_cliente(self, id_cliente, cliente):
+    cursor.execute("UPDATE clientes SET nome=%(nome)s, idade=%(idade)s WHERE idcliente=%(id_cliente)s",
+                   ({'nome': cliente.nome,
+                     'idade': cliente.idade,
+                     'id_cliente': id_cliente})
+
+def remover_cliente(self, id_cliente):
+    cursor.execute("DELETE FROM cliente WHERE idcliente=%s", (id_cliente, ))
+
+listar_clientes()
+inserir_cliente(cliente)
 
 db.close()
