9c402ab3ffa0e78d4d4e09f2f7d56ec2e2742e1c
CAFE2l <pciskolargx@gmail.com>
2026-02-20 16:00:57 -0300

    class 6 finished

--- Aulas/Aula06/index.py
+++ Aulas/Aula06/index.py
@@ -10,5 +10,13 @@ db = MySQLdb.connect(
 )
 cursor = db.cursor()
 
+cursor.executemany("INSERT INTO clientes (nome, idade) VALUES(%s, %s)",
+                   (
+                        ('jose', 50),
+                        ('maria', 51),
+                        ('pedro', 41),
+                        ('fabio filho', 20)
+                       ))
 
 
+db.close()
