6677c1f105beb6f39d6881c4c67f2643ebec18a9
CAFE2l <pciskolargx@gmail.com>
2026-02-20 15:01:16 -0300

    finished third class

--- Aulas/Aula03/index.py
+++ Aulas/Aula03/index.py
@@ -5,19 +5,27 @@ db = MySQLdb.connect(
     passwd="SenhaForte123!",
     db="treinaweb_clientes",
     host="localhost",
-    port=3306
+    port=3306,
+    autocommit=False
 )
 cursor = db.cursor()
 cursor.execute("SELECT * FROM clientes")
 print(cursor.fetchall())
-print("conexao realizda com sucesso")
-cursor.execute("INSERT INTO clientes (nome, idade) VALUES ('luis gustavo', 16)")
-
-
-
-print(cursor.lastrowid)
-cursor.execute("UPDATE clientes SET nome='Ana' WHERE idcliente=1")
+print("conexaao realizda com sucesso")
+try:
+    db.begin()
+    cursor.execute("INSERT INTO clientes (nome, idade) VALUES ('luis gustavo', 16)")
+    cursor.execute("INSERT INTO clientes (nome, idade) VALUES ('cavalo', 122)")
+    db.commit()
+except: 
+    db.rollback()
 cursor.execute("SELECT * FROM clientes")
-print(cursor.fetchall())
+#print(cursor.lastrowid)
+#cursor.execute("UPDATE clientes SET nome='Ana' WHERE idcliente=1")
+#cursor.execute("DELETE FROM clientes WHERE nome='luis gustavo'")
+#cursor.execute("SELECT * FROM clientes")
+#print(cursor.fetchall())
+
+
 db.close()
 
